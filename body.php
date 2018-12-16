2209fe4d08a452671b2f855644613abc2b6b9de3
Nikolay Kapu <dev@kapunik.ru>
2018-12-17 00:36:25 +0300

    разделение индексного файла на части.
    набросок разметки

---
+++ body.php
@@ -0,0 +1,53 @@
+<body>
+<section class="aside">
+  <div class="team_info card grey darken-1">
+    <div class="card-content white-text">
+          <span class="card-title">Пельменный орден — <b>90</b></span>
+          <li>1 раунд - 30 баллов</li>
+          <li>2 раунд - 15 баллов</li>
+          <li>3 раунд - 45 баллов</li>
+        </div>
+  </div>
+
+  <div class="team_info card grey darken-1">
+    <div class="card-content white-text">
+          <span class="card-title">Воронежские мармоны — <b>90</b></span>
+          <li>1 раунд - 30 баллов</li>
+          <li>2 раунд - 15 баллов</li>
+          <li>3 раунд - 45 баллов</li>
+        </div>
+  </div>
+
+  <div class="team_info card grey darken-1">
+    <div class="card-content white-text">
+          <span class="card-title">Давай выпьем — <b>90</b></span>
+          <li>1 раунд - 30 баллов</li>
+          <li>2 раунд - 15 баллов</li>
+          <li>3 раунд - 45 баллов</li>
+        </div>
+  </div>
+
+  <div class="team_info card grey darken-1">
+    <div class="card-content white-text">
+          <span class="card-title">Да нормальная команда — <b>90</b></span>
+          <li>1 раунд - 30 баллов</li>
+          <li>2 раунд - 15 баллов</li>
+          <li>3 раунд - 45 баллов</li>
+        </div>
+  </div>
+
+</section>
+
+<section class="main">
+  <h1 class="round_number">Раунд 1. Как-то называется</h1>
+
+  <div class="round_discription">
+    Условия первого раунда. Количество вопросов, время на раздумья, и тыр-пыр
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+
+  <button type="button" name="button" class="waves-effect waves-light btn-large">
+    <i class="material-icons left"> play_circle_filled </i>
+    Поехали
+  </button>
+</section>
--- index.php
+++ index.php
@@ -1,30 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!--  иконки -->
-  <link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png">
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="mask-icon" href="img/icons/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
+<?php
+include_once 'head.php';
+ ?>
 
-  <!--  наши стили и скрипты -->
-  <link rel="stylesheet" href="css/main.css">
-  <script src="js/main.js" charset="utf-8"></script>
+ <?php
+ include_once 'body.php';
+  ?>
 
-  <!--  материал стили и скрипты-->
-  <link rel="stylesheet" href="css/materialize.css">
-  <script src="js/materialize.js" charset="utf-8"></script>
-  <link href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=cyrillic" rel="stylesheet">
 
-  <title>Quiz</title>
-</head>
-<body>
-<h1>Hello Quiz!</h1>
-</body>
-</html>
+
+<?php
+include_once 'footer.php';
+ ?>
