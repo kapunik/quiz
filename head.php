62ee74d857fd3fbaeb35d361be02e7d619daeeb8
Nikolay Kapu <dev@kapunik.ru>
2018-12-17 23:11:07 +0300

    add todo

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,6 +23,6 @@
   <!--  наши стили и скрипты -->
   <link rel="stylesheet" href="css/main.css?a=<?php echo time(); ?>">
   <script src="js/main.js?a=<?php echo time(); ?>" charset="utf-8"></script>
-  
+
   <title>Quiz</title>
 </head>
