7e2c43f5493806df9a400c2514391ea04f82ecfd
Nikolay Kapu <dev@kapunik.ru>
2018-12-25 01:54:18 +0300

    mvc немножк

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,3 +1,59 @@
+<button id="btn_hide_aside" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">arrow_back</i></button>
+
+<section class="aside">
+
+
+
+    <div class="team_info">
+        <i class="material-icons star star-gold">stars</i>
+        <div class="taem_info__title">
+            <span class="team_info__name">Пельменный орден</span> — <b class="team_info__score">90</b>
+        </div>
+        <ul class="team_info__rounds_scope">
+            <li>1 раунд - 30</li>
+            <li>2 раунд - 15</li>
+            <li>3 раунд - 45</li>
+        </ul>
+    </div>
+
+    <div class="team_info">
+        <i class="material-icons star star-silver">stars</i>
+        <div class="taem_info__title">
+            <span class="team_info__name">Воронежские мормоны</span> — <b class="team_info__score">90</b>
+        </div>
+        <ul class="team_info__rounds_scope">
+            <li>1 раунд - 30</li>
+            <li>2 раунд - 15</li>
+            <li>3 раунд - 45</li>
+        </ul>
+    </div>
+
+    <div class="team_info">
+        <i class="material-icons star star-bronze">stars</i>
+        <div class="taem_info__title">
+            <span class="team_info__name">Давай выпьем</span> — <b class="team_info__score">90</b>
+        </div>
+        <ul class="team_info__rounds_scope">
+            <li>1 раунд - 30</li>
+            <li>2 раунд - 15</li>
+            <li>3 раунд - 45</li>
+        </ul>
+    </div>
+
+    <div class="team_info">
+        <div class="taem_info__title">
+            <span class="team_info__name">Да нормальная команда</span> — <b class="team_info__score">90</b>
+        </div>
+        <ul class="team_info__rounds_scope">
+            <li>1 раунд - 30</li>
+            <li>2 раунд - 15</li>
+            <li>3 раунд - 45</li>
+        </ul>
+    </div>
+
+</section>
+
+<section class="main">
 <h1 class="round_number">Раунд 1. Разминка</h1>
 <div class="rond__short_info">
   <span class="short_info__questions">
@@ -19,3 +75,4 @@
     <i class="material-icons left "> play_circle_filled </i>
     Поехали
 </button>
+</section>
