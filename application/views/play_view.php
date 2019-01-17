eed99b7f0d4fe96699a6dd145c1cacbdc1837cca
Nikolay Kapu <dev@kapunik.ru>
2019-01-17 22:44:43 +0300

    захуярил вьюшку ахуенную (play)

--- application/views/play_view.php
+++ application/views/play_view.php
@@ -1,2 +1,32 @@
-<?php
-vardump($data);
+<div class="timer mel">
+30
+</div>
+
+<div class="flex-column mel darken-transparent list">
+  <div class="list_questions">1</div>
+  <div class="list_questions">2</div>
+  <div class="list_questions">3</div>
+  <div class="list_questions">4</div>
+  <div class="list_questions">5</div>
+  <div class="list_questions">6</div>
+  <div class="list_questions">7</div>
+  <div class="list_questions">8</div>
+  <div class="list_questions">9</div>
+  <div class="list_questions">10</div>
+</div>
+<div class="flex-column mel darken-transparent">
+<h1 class="star-gold">ИГРА 1 РАУНД 1</h1>
+<section class="description">
+  Проекция на подвижные оси представляет собой ультраосновной секстант. Момент сил, а там действительно могли быть видны звезды, о чем свидетельствует Фукидид искажает словесный сброс. Обсценная идиома трансформирует одиннадцатисложник, несмотря на отсутствие единого пунктуационного алгоритма.
+
+Векторная форма жизненно интегрирует мелодический восход . Стихотворение типично. Отсутствие трения меняет центральный оз.
+</section>
+
+<section class="answers flex_wrap">
+  <div class="answer">ХУЙ</div>
+  <div class="answer">hui</div>
+  <div class="answer">hui</div>
+  <div class="answer">вышел саня из бытовки в бар пошел быхать конину</div>
+</section>
+
+</div>
--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
 $(document).ready(function() {
 
-$('#roundStart').on('click', function(){
+  $('#roundStart').on('click', function() {
 
-  $.ajax({
-  	            type: 'post',
-  	            url: "templates/question_with_four_answers.php", //Путь к обработчику
-  	            data: ' ',
-  	            response: 'text',
-  	            success: function(data){
-  	                $(".main").html(data); //Выводим полученые данные в списке
-  	           }
-  	       });
+    $.ajax({
+      type: 'post',
+      url: "templates/question_with_four_answers.php", //Путь к обработчику
+      data: ' ',
+      response: 'text',
+      success: function(data) {
+        $(".main").html(data); //Выводим полученые данные в списке
+      }
+    });
 
-});
+  });
 
-$('#btn_hide_aside').on('click', function(){
-  $('.aside').toggleClass('flatten');
-  // $('.aside').slideToggle();
-  $('#btn_hide_aside').toggleClass('rotate180');
-});
+  $('#btn_hide_aside').on('click', function() {
+    $('.aside').toggleClass('flatten');
+    // $('.aside').slideToggle();
+    $('#btn_hide_aside').toggleClass('rotate180');
+  });
+
+
+  function timer_start(sec) {
+    $('.timer').addClass('alarm');
+
+    var start = sec;
+
+    var timerId = setInterval(function() {
+      start = start - 1;
+      $('.timer').html(start);
+    }, 1000);
+
+    var stop = sec * 1000;
+
+    setTimeout(function() {
+      clearInterval(timerId);
+        $('.timer').addClass('spartak');
+    }, stop);
+
+  }
+
+  $('.timer').on('click', function() {
+    timer_start(30);
+  });
 
 });
