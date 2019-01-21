2a18bab3a4e4db3c2b4968c89d8c299c825bbac5
Vavas <radin-vowa@yandex.ru>
2019-01-21 22:49:32 +0300

    Не особо удачно, надо подумать

--- application/views/result_round_view.php
+++ application/views/result_round_view.php
@@ -1,1 +1,15 @@
-результаты крч
+<div class="mel">
+  <h1>Внимание! Ознакомьтесь с правильными ответами!</h1>
+</div>
+<section class="mel">
+  <div class="list_questions">1</div>
+  <div class="list_questions">2</div>
+  <div class="list_questions">3</div>
+  <div class="list_questions">4</div>
+  <div class="list_questions">5</div>
+  <div class="list_questions">6</div>
+  <div class="list_questions">7</div>
+  <div class="list_questions">8</div>
+  <div class="list_questions">9</div>
+  <div class="list_questions">10</div>
+</section>
